53c65df58172a19bbbb4af173453fc1f7c7d66c4
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2020-01-13 15:56:08 +0100

    Removing logs

--- src/shopService.js
+++ src/shopService.js
@@ -4,16 +4,14 @@ const { findMatchingSku } = require("./utils/findMatchingSku");
 const { setInventoryLevel } = require("./api/v2/InventoryLevelApi");
 
 const syncTwoShopsByItem = async (item, currentShop, shopToSync) => {
-  console.log('item:'+ item);
   const productSku = await getItemSku(item, currentShop);
-  console.log('productSku:'+ productSku);
   const products = await getAll(shopToSync);
-  console.log('products:'+ products);
+
   const inventoryItemId =  await findMatchingSku(
     products,
     productSku
   );
-    console.log(inventoryItemId);
+  
   await setInventoryLevel(shopToSync, inventoryItemId, item);
 
 };
